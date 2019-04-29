79fb483d46613927d9cfda8bf64d79504dad8184
JiangJun <junzi528@163.com>
2019-04-29 11:52:21 +0800

    composer包学习

---
+++ src/User/client.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace User\Client;
+class Client
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index ()
+    {
+        echo "this is client";
+    }
+
+}
